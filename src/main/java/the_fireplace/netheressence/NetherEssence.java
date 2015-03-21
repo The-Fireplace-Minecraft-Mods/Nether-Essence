5f4bd5ebaa1987085f546065e1fef4ccd8e3041a
The_Fireplace <the.f1repl4ce@gmail.com>
2015-03-21 02:00:22 -0500

    Updated version number

--- src/main/java/the_fireplace/netheressence/NetherEssence.java
+++ src/main/java/the_fireplace/netheressence/NetherEssence.java
@@ -40,7 +40,7 @@ public class NetherEssence
     
     public static final String MODID = "netheressence";
     public static final String MODNAME = "Nether Essence";
-    public static final String VERSION = "2.0.0.1";
+    public static final String VERSION = "2.0.1.0";
 	
 	private static int updateNotification;
 	private static String releaseVersion;
