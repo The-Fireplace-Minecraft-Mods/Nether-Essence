47771a11aa8e8e87eb363364187693dcdad108e9
The_Fireplace <the.f1repl4ce@gmail.com>
2017-10-07 14:24:04 -0500

    Begin the 1.12 port. Recipes still need work

--- src/main/scala/the_fireplace/netheressence/NetherEssence.scala
+++ src/main/scala/the_fireplace/netheressence/NetherEssence.scala
@@ -2,22 +2,27 @@ package the_fireplace.netheressence
 
 import java.lang.{Character => JChar}
 
+import net.minecraft.block.Block
 import net.minecraft.block.material.Material
 import net.minecraft.client.renderer.block.model.ModelResourceLocation
 import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.init.{Blocks, Items}
 import net.minecraft.item.{Item, ItemBlock, ItemStack}
+import net.minecraftforge.client.event.ModelRegistryEvent
 import net.minecraftforge.client.model.ModelLoader
-import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.event.RegistryEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.Mod.EventHandler
 import net.minecraftforge.fml.common.event.{FMLInitializationEvent, FMLPreInitializationEvent}
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.registry.GameRegistry
+import net.minecraftforge.fml.relauncher.{Side, SideOnly}
 import net.minecraftforge.oredict.ShapedOreRecipe
+import net.minecraftforge.registries.IForgeRegistry
 import the_fireplace.netheressence.blocks.{BlockItemRecovery, BlockNetherEssence, BlockRadiantNetherEssence}
-import the_fireplace.netheressence.handlers.{ForgeEvents, NetherEssenceFuelHandler}
 
-@Mod(modid = NetherEssence.MODID, name = NetherEssence.MODNAME, modLanguage = "scala", updateJSON = "http://thefireplace.bitnamiapp.com/jsons/netheressence.json", acceptedMinecraftVersions = "[1.11,)")
+@Mod(modid = NetherEssence.MODID, name = NetherEssence.MODNAME, modLanguage = "scala", acceptedMinecraftVersions = "[1.12,1.13)")
+@Mod.EventBusSubscriber
 object NetherEssence {
 	final val MODID = "netheressence"
 	final val MODNAME = "Nether Essence"
@@ -26,30 +31,13 @@ object NetherEssence {
 		override def getTabIconItem: ItemStack = new ItemStack(NetherEssence.nether_essence)
 	}
 
-	val nether_essence = new Item().setCreativeTab(NetherEssence.tabNetherEssence)
-		.setUnlocalizedName("NetherDust")
+	val nether_essence = new Item().setCreativeTab(NetherEssence.tabNetherEssence).setUnlocalizedName("NetherDust").setRegistryName("NetherDust")
 
-	val nether_essence_block = new BlockNetherEssence(Material.ROCK)
+	val nether_essence_block = new BlockNetherEssence(Material.ROCK).setRegistryName("NetherDustBlock")
 
-	val radiant_nether_essence_block = new BlockRadiantNetherEssence(Material.ROCK)
+	val radiant_nether_essence_block = new BlockRadiantNetherEssence(Material.ROCK).setRegistryName("radiant_nether_essence_block")
 
-	val item_recovery_block = new BlockItemRecovery(Material.ROCK)
-
-	@EventHandler
-	def preInit(event: FMLPreInitializationEvent) {
-		GameRegistry.register(nether_essence_block.setRegistryName("NetherDustBlock"))
-		GameRegistry.register(new ItemBlock(nether_essence_block).setRegistryName("NetherDustBlock"))
-		GameRegistry.register(item_recovery_block.setRegistryName("item_recovery_block"))
-		GameRegistry.register(new ItemBlock(item_recovery_block).setRegistryName("item_recovery_block"))
-		GameRegistry.register(nether_essence.setRegistryName("NetherDust"))
-		GameRegistry.register(radiant_nether_essence_block.setRegistryName("radiant_nether_essence_block"))
-		GameRegistry.register(new ItemBlock(radiant_nether_essence_block).setRegistryName("radiant_nether_essence_block"))
-		GameRegistry.registerFuelHandler(new NetherEssenceFuelHandler)
-		MinecraftForge.EVENT_BUS.register(new ForgeEvents)
-		if (event.getSide.isClient) {
-			registerItemRenders()
-		}
-	}
+	val item_recovery_block = new BlockItemRecovery(Material.ROCK).setRegistryName("item_recovery_block")
 
 	@EventHandler
 	def load(event: FMLInitializationEvent) {
@@ -77,7 +65,48 @@ object NetherEssence {
 		GameRegistry.addShapelessRecipe(dustStack8, radNetherEssenceBlockStack)
 	}
 
-	def registerItemRenders() {
+	var blockRegistry: IForgeRegistry[Block] = _
+
+	def registerBlock(block: Block) {
+		if (blockRegistry == null)
+			return
+		blockRegistry.register(block)
+	}
+
+	var itemRegistry: IForgeRegistry[Item] = _
+
+	def registerItem(item: Item) {
+		if (itemRegistry == null)
+			return
+		itemRegistry.register(item)
+	}
+
+	def registerItemForBlock(block: Block) {
+		if (itemRegistry == null)
+			return
+		itemRegistry.register(new ItemBlock(block).setRegistryName(block.getRegistryName))
+	}
+
+	@SubscribeEvent
+	def registerBlocks(event: RegistryEvent.Register[Block]) {
+		blockRegistry = event.getRegistry
+		registerBlock(nether_essence_block)
+		registerBlock(radiant_nether_essence_block)
+		registerBlock(item_recovery_block)
+	}
+
+	@SubscribeEvent
+	def registerItems(event: RegistryEvent.Register[Item]) {
+		itemRegistry = event.getRegistry
+		registerItem(nether_essence)
+		registerItemForBlock(nether_essence_block)
+		registerItemForBlock(radiant_nether_essence_block)
+		registerItemForBlock(item_recovery_block)
+	}
+
+	@SideOnly(Side.CLIENT)
+	@SubscribeEvent
+	def registerItemRenders(event: ModelRegistryEvent) {
 		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(nether_essence_block), 0, new ModelResourceLocation(NetherEssence.MODID + ":NetherDustBlock", "inventory"))
 		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(item_recovery_block), 0, new ModelResourceLocation(NetherEssence.MODID + ":item_recovery_block", "inventory"))
 		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(radiant_nether_essence_block), 0, new ModelResourceLocation(NetherEssence.MODID + ":radiant_nether_essence_block", "inventory"))
--- src/main/scala/the_fireplace/netheressence/handlers/ForgeEvents.scala
+++ src/main/scala/the_fireplace/netheressence/handlers/ForgeEvents.scala
@@ -1,23 +1,38 @@
 package the_fireplace.netheressence.handlers
 
+import net.minecraft.item.Item
 import net.minecraft.nbt.NBTTagCompound
 import net.minecraftforge.event.entity.item.ItemExpireEvent
+import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent
 import net.minecraftforge.event.world.WorldEvent
+import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import the_fireplace.netheressence.NetherEssence
 
 /**
 	* @author The_Fireplace
 	*/
-class ForgeEvents {
+@Mod.EventBusSubscriber(modid = NetherEssence.MODID)
+object ForgeEvents {
 	@SubscribeEvent
 	def stackExpire(event: ItemExpireEvent) {
-		if (!event.isCanceled && !event.getEntity.world.isRemote && !event.getEntityItem.getEntityItem.isEmpty && !event.getEntityItem.cannotPickup) {
-			DeadStackData.getInstance.deadStacks.add(event.getEntityItem.getEntityItem.writeToNBT(new NBTTagCompound).toString)
+		if (!event.isCanceled && !event.getEntity.world.isRemote && !event.getEntityItem.getItem.isEmpty && !event.getEntityItem.cannotPickup) {
+			DeadStackData.getInstance.deadStacks.add(event.getEntityItem.getItem.writeToNBT(new NBTTagCompound).toString)
 		}
 	}
 
 	@SubscribeEvent
 	def worldSave(event: WorldEvent.Unload) {
 		DeadStackData.save()
+	}
+
+	@SubscribeEvent
+	def fuelEvent(event: FurnaceFuelBurnTimeEvent) {
+		if (event.getItemStack.getItem == NetherEssence.nether_essence)
+			event.setBurnTime(800)
+		else if (event.getItemStack.getItem == Item.getItemFromBlock(NetherEssence.nether_essence_block))
+			event.setBurnTime(6400)
+		else if (event.getItemStack.getItem == Item.getItemFromBlock(NetherEssence.radiant_nether_essence_block))
+			event.setBurnTime(6200)
 	}
 }
